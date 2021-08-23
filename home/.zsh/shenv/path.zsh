63a68ca191aeab610245e6c9e5ee04562680f348
Mike Barker <mike@thebarkers.com>
2021-08-23 16:00:46 -0600

    Fix pyenv init
    Fix #12

--- home/.zsh/shenv/path.zsh
+++ home/.zsh/shenv/path.zsh
@@ -5,12 +5,12 @@
 # July 24th, 2019
 
 # Create an array of potential paths to become the new path
-newpath+=(~/bin)
-newpath+=(~/bin/flutter/bin)
-newpath+=(~/.local/bin)
-newpath+=(~/Library/Python/3.7/bin)
-newpath+=(~/Library/Python/2.7/bin)
+[[ -x ~/bin ]] && newpath+=(~/bin)
+[[ -x ~/.local/bin ]] && newpath+=(~/.local/bin)
+
 newpath+=($path)
+
+[[ -x /usr/local/sbin ]] && newpath+=(/usr/local/sbin)
 
 # Compose the path from the array of proposed paths.
 # Only existing paths will be added to the new path.
