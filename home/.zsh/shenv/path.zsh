c448887999f0161013290763aff8be04967d8ebd
Mike Barker <mike@thebarkers.com>
2022-03-31 09:15:42 -0600

    Add macos opt folders to path

--- home/.zsh/shenv/path.zsh
+++ home/.zsh/shenv/path.zsh
@@ -7,6 +7,8 @@
 # Create an array of potential paths to become the new path
 [[ -x ~/bin ]] && newpath+=(~/bin)
 [[ -x ~/.local/bin ]] && newpath+=(~/.local/bin)
+[[ -x /opt/homebrew/bin ]] && newpath+=(/opt/homebrew/bin)
+[[ -x /opt/microsoft/bin ]] && newpath+=(/opt/microsoft/bin)
 
 newpath+=($path)
 
